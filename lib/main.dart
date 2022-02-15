a28d3c9c3ccc1e96b3aea3883988a4b1452c5a08
Francisc0HLL <j.francisco.hll@gmail.com>
2022-02-15 20:43:42 -0300

    second commit, create card and add icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:disenos_1/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,29 @@
-import 'package:disenos_1/widgets/background.dart';
 import 'package:flutter/material.dart';
+
+import 'package:disenos_1/widgets/background.dart';
+import 'package:disenos_1/widgets/card_table.dart';
+import 'package:disenos_1/widgets/custom_bottom_navigation.dart';
+import 'package:disenos_1/widgets/page_title.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [Backgound()],
-    ));
+      body: Stack(
+        children: [Backgound(), _Homebody()],
+      ),
+      bottomNavigationBar: CustomBottomNavigation(),
+    );
+  }
+}
+
+class _Homebody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [PageTitle(), CardTable()],
+      ),
+    );
   }
 }
