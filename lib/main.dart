836cbd09e2c360b6afecd2598749a2ec375fa110
Francisc0HLL <j.francisco.hll@gmail.com>
2022-02-15 18:36:34 -0300

    first commit, background of the app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:disenos_1/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'home_screen',
+      routes: {
+        'home_screen': (_) => HomeScreen(),
+      },
+    );
+  }
+}
