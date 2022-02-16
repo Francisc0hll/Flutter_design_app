f121785cb6c2a2009516e297fabba09b7c10a189
Francisc0HLL <j.francisco.hll@gmail.com>
2022-02-15 21:07:18 -0300

    third commit, creat blur of cards

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
+      theme: ThemeData.dark(),
       initialRoute: 'home_screen',
       routes: {
         'home_screen': (_) => HomeScreen(),
